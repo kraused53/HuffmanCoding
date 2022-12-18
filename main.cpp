f3ceeb51ccd34f69e08c8899485f36ccf8b054ec
kraused53 <kraused53@gmail.com>
2022-12-18 11:52:39 -0500

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "llist.h"
+
+int main(int argc, char **argv) {
+    if(argc != 2) {
+        std::cout << "Please provide the path to a .txt file.\n";
+    }
+
+    // Create new, empty list
+    Node *head = NULL;
+
+    // Open file
+    std::ifstream dataFile;
+    dataFile.open(argv[1]);
+
+    // Walk throug file line by line
+    std::string line;
+    while(std::getline(dataFile, line)) {
+        llist_append(&head, stoi(line));
+    }
+
+    // Close data file
+    dataFile.close();
+
+    // Sort the list
+    llist_sort(&head);
+
+    // Print the unsorted list
+    llist_print_for_file(head);
+
+    // Free the list
+    llist_delete(&head);
+
+    return 0;
+}
