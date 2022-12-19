a351966dab5abeb21c4adecde5df952ae5afb5c7
kraused53 <kraused53@gmail.com>
2022-12-19 08:39:21 -0500

    Began work on binary tree class
    - added function to insert a node into a binary search tree
    
    - added function to search a binary tree for a given node

--- btree.cpp
+++ btree.cpp
@@ -1,1 +1,79 @@
 #include "btree.h"
+
+
+/*
+ * Name:
+ *      btree_insert_node_search_tree
+ * 
+ * Inputs:
+ *      btNode *root
+ *          - Pointer to the root node of a binary search tree
+ *      int data
+ *          - The value to be inserted into the tree
+ *  
+ * Outputs:
+ *      btNode *
+ *          - Retruns a pointer to the root of a binary search tree with the
+ *              given value added
+ * 
+ * Description:
+ *     Recursivly searches a given binary search tree for the proper place to 
+ *      add the given value, then adjusts the tree to allow a new node to 
+ *      be inserted.
+ * 
+ */
+btNode *btree_insert_node_serach_tree(btNode *root, int data) {
+
+    // If given an empty tree
+    if(root == NULL) {
+        root = new btNode(data);
+        return root;
+    }
+
+    // If given value is greater than value of current root,
+    //      recursivly search right branch for a spot to insert node
+    if(data > root->data) {
+        root->right = btree_insert_node_serach_tree(root->right, data);
+    }else if(data < root->data) {
+        // If given value is less than value of current root,
+        //      recursivly search left branch for a spot to insert node
+        root->left = btree_insert_node_serach_tree(root->left, data);
+    }
+
+    return root;
+}
+
+
+/*
+ * Name:
+ *      btree_search_search_tree
+ * 
+ * Inputs:
+ *      btNode *root
+ *          - Pointer to the root node of a binary search tree
+ *      int key
+ *          - The value to be searched for
+ *  
+ * Outputs:
+ *      btNode *
+ *          - Retruns a pointer to the node with the given search value
+ * 
+ * Description:
+ *     Recursivly searches a given binary search tree for a node containing 
+ *          the given value.
+ * 
+ */
+btNode *btree_search_serach_tree(btNode *root, int key) {
+    // If given an empty tree or if root->data == key, return root
+    if((root == NULL) || (root->data == key)) {
+        return root;
+    }
+
+    // If key is more than root, search right sub-tree
+    if(key > root->data) {
+        return btree_search_serach_tree(root->right, key);
+    }
+
+    // If key is less than root, search left sub-tree
+    return btree_search_serach_tree(root->left, key);
+}
--- btree.h
+++ btree.h
@@ -1,4 +1,23 @@
 #ifndef BTREE_H
     #define BTREE_H
 
+    #include <iostream>
+
+    // Define Binary Tree btNode
+    class btNode {
+        public:
+            int data;
+            btNode *left;
+            btNode *right;
+
+            btNode(int new_data) {
+                data = new_data;
+                left = NULL;
+                right = NULL;
+            }
+    };
+
+    /*** <-- Function Prototypes --> ***/
+    btNode *btree_insert_node_serach_tree(btNode *root, int data);
+    btNode *btree_search_serach_tree(btNode *root, int key);
 #endif
