5168993ce2b56b6e1db5032ea0566679d6c398a1
kraused53 <kraused53@gmail.com>
2022-12-28 10:41:37 -0500

    Cleared main, as all of my old functions are gone

--- main.cpp
+++ main.cpp
@@ -2,13 +2,12 @@
 #include <fstream>
 
 
-
 int main(int argc, char **argv) {
-    if(argc < 2) {
-        std::cout << "Please provide the path to a .txt file and runtime settings\n";
+    if(argc != 2) {
+        std::cout << "Please provide the path to a .txt file\n";
     }
 
-
+    
     return 0;
 }
 
